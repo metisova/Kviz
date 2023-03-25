15ab8aee8ae4b64c6094393f9b652948e17d6688
Anna Kopylova <anna.metisova@gmail.com>
2023-03-25 11:28:45 +0100

    Hotove zadani

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('funguju!');
+
+const questionElm = document.querySelector('.question');
+questionElm.textContent =
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.';
+
+const answer = prompt(
+  'Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon:',
+);
+
+const answerTextElm = document.querySelector('.answer__text');
+answerTextElm.textContent = answer;
+
+const answerElm = document.querySelector('.answer');
+
+if (answer === 'Bell') {
+  answerElm.classList.add('answer--correct');
+} else {
+  answerElm.classList.add('answer--wrong');
+}
